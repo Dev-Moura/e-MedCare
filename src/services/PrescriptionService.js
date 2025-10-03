81d814a432e748c6e8af5f73f8952eacb293507d
Michael de Souza <113400472+Dev-Moura@users.noreply.github.com>
2025-10-03 18:50:34 -0300

    update

---
+++ src/services/PrescriptionService.js
@@ -0,0 +1,86 @@
+import prescription from "../models/Prescription.js";
+import PrescriptionRepository from "../repository/PrescriptionRepository.js";
+import AppointmentService from "./AppointmentService.js";
+import PacientService from "./PacientService.js";
+import DoctorService from "./DoctorService.js";
+import fs from "fs";
+import PDFDocument from "pdfkit";
+
+const getAllPrescriptions = async () => {
+  return PrescriptionRepository.getAllPrescriptions();
+};
+
+const getPrescription = async (id) => {
+  return PrescriptionRepository.getPrescription(id);
+};
+
+const savePrescription = async ({
+  date,
+  appointmetId,
+  medicine,
+  dosage,
+  instructions,
+}) => {
+  return PrescriptionRepository.savePrescription({
+    date,
+    appointmetId,
+    medicine,
+    dosage,
+    instructions,
+  });
+};
+
+const updatePrescription = async (
+  id,
+  { date, appointmetId, medicine, dosage, instructions, file }
+) => {
+  return PrescriptionRepository.updatePrescription(id, {
+    date,
+    appointmetId,
+    medicine,
+    dosage,
+    instructions,
+    file,
+  });
+};
+
+const deletePrescription = async (id) => {
+  return PrescriptionRepository.deletePrescription(id);
+};
+
+const generatePresciptionFile = async (prescription) => {
+  const appointment = await AppointmentService.getAppointment(
+    prescription.appointmentId
+  );
+  const pacient = await PacientService.getPacient(appointment.pacientId);
+  const doctor = await DoctorService.getDoctor(appointment.doctorId);
+
+  const id = prescription._id;
+  const document = new PDFDocument({ font: "Courier" });
+  const filePath = "./E-MEDCARE/prescription/" + id + ".pdf";
+
+  document.pipe(fs.createWriteStream(filePath));
+  document.fontSize(16).text("Pacient Name: " + pacient.name);
+  document.fontSize(16).text("Doctor Name: " + doctor.name);
+
+  const recipe = "Medicine: " + prescription.medicine;
+  document.fontSize(12).text(recipe);
+
+  document.fontSize(12).text("Dosage: " + prescription.dosage);
+  document.fontSize(12).text("Instructions: " + prescription.dosage);
+
+  document.end();
+
+  return prescription;
+};
+
+const prescriptionService = {
+  getAllPrescriptions,
+  getPrescription,
+  savePrescription,
+  updatePrescription,
+  deletePrescription,
+  generatePresciptionFile,
+};
+
+export default prescriptionService;
