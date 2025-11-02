bc160279b319dcda7dacc766d8baca33bffd41a4
Michael de Souza <113400472+Dev-Moura@users.noreply.github.com>
2025-11-02 09:11:43 -0300

    working on login screen

--- emedcare-app/src/app/page.tsx
+++ emedcare-app/src/app/page.tsx
@@ -38,51 +38,60 @@ export default function Home() {
 
   return (
     <>
-      <div className=" flex justify-center items-center h-screen bg-gray-350 ">
-        <form
-          className="w-full max-w-sm p-10 bg-white rounded-lg shadow-md "
-          onSubmit={authenticantion}
-        >
-          <h1 className="font-bold-text-blue-500 py-2 block text-2x1">Login</h1>
-          <div className="w-full py-2">
-            <label htmlFor="" className="text-sm font-bold py-2 block">
-              Usuário
-            </label>
-            <input
-              type="text"
-              name="name"
-              className="w-full border-1 border-gray-200 p-2 rounded-sm"
-              onChange={(e: any) => setLogin(e.target.value)}
-            />
-          </div>
-          <div className="w-full py-2">
-            <label htmlFor="" className="text-sm font-bold py-2 block">
-              Senha
-            </label>
-            <input
-              name="login"
-              type="password"
-              className="w-full border-[1px] border-gray-200 p-2 rounded-sm"
-              onChange={(e: any) => setPassword(e.target.value)}
-            />
-          </div>
-          <div className="w-full py-2">
-            <button className="w-20 p-2 text-#f2f2f2 border-gray-200 border-[1px] rounded-sm bg-blue-400">
-              Login
-            </button>
-          </div>
-          <div>
-            {error && (
-              <div
-                className="p-2 text-white border-gray-200 border-[1px] rounded-sm bg-red-400"
-                style={{ color: "red" }}
-              >
-                {error}
-              </div>
-            )}
-          </div>
-        </form>
-      </div>
+        <header className= " w-full h-16" >
+        <h1 className= " flex w-full text-4xl">E-medCare</h1>
+        </header>
+        <main className="">
+            <div className=" w-full h-full flex justify-center items-center h-screen bg-gray-200 ">
+                <form
+                    className="w-full max-w-sm p-10 bg-white rounded-lg shadow-md "
+                    onSubmit={authenticantion}
+                >
+                    <h1 className="font-bold py-6 block text-4xl ">Entrar</h1>
+                    <div className="w-full py-2">
+                        {/*<label htmlFor="" className="text-sm font-bold py-2 block">*/}
+                        {/*</label>*/}
+                        <input
+                            placeholder="Login"
+                            type="text"
+                            name="name"
+                            className="w-full border-1 border-gray-600 p-4 rounded-sm placeholder-gray-600"
+                            onChange={(e: any) => setLogin(e.target.value)}
+                        />
+                    </div>
+                    <div className="w-full py-2">
+                        {/*<label htmlFor="" className="text-sm font-bold p-2 block">*/}
+
+                        {/*</label>*/}
+                        <input
+                            placeholder="Senha"
+                            name="login"
+                            type="password"
+                            className="mt-2 w-full border-1 border-gray-600 p-4 rounded-sm placeholder-gray-600"
+                            onChange={(e: any) => setPassword(e.target.value)}
+                        />
+                    </div>
+                    <div className="w-full py-2">
+                        <a className=" text-blue-400">Esqueceu a senha?</a>
+
+                        <button className=" mt-4 w-full p-2 text-gray-200 border rounded-4xl bg-blue-400 flex justify-center ">
+                            Login
+                        </button>
+                    </div>
+                    <div>
+                        {error && (
+                            <div
+                                className="p-2 text-white border-gray-200 border rounded-sm bg-red-400"
+                                style={{ color: "red" }}
+                            >
+                                {error}
+                            </div>
+                        )}
+                    </div>
+                </form>
+            </div>
+        </main>
+
     </>
   );
 }
